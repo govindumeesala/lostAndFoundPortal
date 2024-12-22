7219ca8a7f9809af1d5d8f95ddbbf19d9f01d2c3
Manideeptheexplorer <venkatkotha828@gmail.com>
2024-12-22 14:40:34 +0530

    ui updated

--- frontend/src/components/footer.jsx
+++ frontend/src/components/footer.jsx
@@ -3,7 +3,7 @@ import "../stylesheets/Homepage.css";
 
 const Footer = () => (
   <footer>
-    <h3>Created by: Sprint Squads</h3>
+    <h3>@Copyright:Sprint Squads</h3>
     <h3>RGUKT Nuzvid,AP</h3>
   </footer>
 );
--- frontend/src/routes/lostPage.jsx
+++ frontend/src/routes/lostPage.jsx
@@ -1,20 +1,25 @@
 /* eslint-disable no-constant-condition */
-import React, { useEffect } from 'react';
-import '../stylesheets/lostPage.css';
-import { Alert, CircularProgress, Grid, TextField } from '@mui/material';
-import Card from '../components/Card';
-import { fetchLost } from '../Api/Data';
-import { UserContext } from '../utils/UserContext';
-import Popup from '../components/popup';
+import React, { useEffect } from "react";
+import "../stylesheets/lostPage.css";
+import { Alert, CircularProgress, Grid, TextField } from "@mui/material";
+import Card from "../components/Card";
+import { fetchLost } from "../Api/Data";
+import { UserContext } from "../utils/UserContext";
+import Popup from "../components/popup";
 
 const LostPage = () => {
   const [lostItems, setLostItems] = React.useState([]);
   const [, , , setPageNumber] = React.useContext(UserContext);
   const [isAlert, setIsAlert] = React.useState(false);
   const [isSuccess, setIsSuccess] = React.useState(false);
-  const [message, setMessage] = React.useState('');
-  const [searchText, setSearchText] = React.useState('');
-  const [searchTags, setSearchTags] = React.useState({ username: false, description: false, title: false, location: false });
+  const [message, setMessage] = React.useState("");
+  const [searchText, setSearchText] = React.useState("");
+  const [searchTags, setSearchTags] = React.useState({
+    username: false,
+    description: false,
+    title: false,
+    location: false,
+  });
   const [searchBtn, setSearchBtn] = React.useState(false);
   const [isLoading, setIsLoading] = React.useState(true);
   const [popupTrigger, setPopupTrigger] = React.useState(false);
@@ -34,99 +39,166 @@ const LostPage = () => {
     }, 1000);
   };
 
-
   useEffect(() => {
     setPageNumber(1);
-    document.addEventListener('keydown', (e) => {
-      if(e.key === 'Escape') {
-        setPopupTrigger(false);
-      }
-    }, false);
+    document.addEventListener(
+      "keydown",
+      (e) => {
+        if (e.key === "Escape") {
+          setPopupTrigger(false);
+        }
+      },
+      false
+    );
   }, []);
 
   useEffect(() => {
     getLostItems();
   }, [searchBtn]);
 
-
-  return (<>
-    {isAlert ? <Alert severity={isSuccess ? 'success' : 'error'} onClose={() => {
-      setIsAlert(false);
-    }}>{message}</Alert> : ''}
-    <section className="lostContainer">
-      <div className='lost-page-section'>
-        <div className='filterBox'>
-          <div className='search-box'>
-            <TextField
-              hiddenLabel
-              id="filled-hidden-label-small"
-              placeholder='Search'
-              variant="outlined"
-              size="small"
-              value={searchText}
-              onChange={(e) => { setSearchText(e.target.value); }}
-            />
-            <button onClick={(e) => { searchBtnHandler(e); }}>Search</button>
+  return (
+    <>
+      {isAlert ? (
+        <Alert
+          severity={isSuccess ? "success" : "error"}
+          onClose={() => {
+            setIsAlert(false);
+          }}
+        >
+          {message}
+        </Alert>
+      ) : (
+        ""
+      )}
+      <section className="lostContainer">
+        <div className="lost-page-section">
+          <div className="filterBox">
+            <div className="search-box">
+              <TextField
+                hiddenLabel
+                id="filled-hidden-label-small"
+                placeholder="Search"
+                variant="outlined"
+                size="small"
+                value={searchText}
+                onChange={(e) => {
+                  setSearchText(e.target.value);
+                }}
+              />
+              <button
+                onClick={(e) => {
+                  searchBtnHandler(e);
+                }}
+              >
+                Search
+              </button>
+            </div>
+            <fieldset>
+              <legend>---- Search Filters ----</legend>
+              <div>
+                <input
+                  type="checkbox"
+                  id="username"
+                  name="username"
+                  value={searchTags.username}
+                  onChange={() => {
+                    setSearchTags({
+                      ...searchTags,
+                      username: !searchTags.username,
+                    });
+                  }}
+                />
+                <label htmlFor="username">UserName</label>
+              </div>
+              <div>
+                <input
+                  type="checkbox"
+                  id="description"
+                  name="description"
+                  value={searchTags.description}
+                  onChange={() => {
+                    setSearchTags({
+                      ...searchTags,
+                      description: !searchTags.description,
+                    });
+                  }}
+                />
+                <label htmlFor="description">Description</label>
+              </div>
+              <div>
+                <input
+                  type="checkbox"
+                  id="title"
+                  name="title"
+                  value={searchTags.title}
+                  onChange={() => {
+                    setSearchTags({ ...searchTags, title: !searchTags.title });
+                  }}
+                />
+                <label htmlFor="title">Title</label>
+              </div>
+              <div>
+                <input type="checkbox" id="lost" name="lost" />
+                <label htmlFor="lost">Lost</label>
+              </div>
+              <div>
+                <input
+                  type="checkbox"
+                  id="date"
+                  name="date"
+                  value={searchTags.location}
+                  onChange={() => {
+                    setSearchTags({
+                      ...searchTags,
+                      location: !searchTags.location,
+                    });
+                  }}
+                />
+                <label htmlFor="date">Location</label>
+              </div>
+            </fieldset>
           </div>
-          <fieldset>
-            <legend>---- Search Filters ----</legend>
-            <div>
-              <input type="checkbox" id="username" name="username" value={searchTags.username} onChange={() => { setSearchTags({ ...searchTags, username: !searchTags.username }); }} />
-              <label htmlFor="username">UserName</label>
+          <div className="filter-box-spacing"></div>
+          <div className={"lostItems"}>
+            <div className="lostpage-banner">
+              <h1>Lost Items</h1>
             </div>
-            <div>
-              <input type="checkbox" id="description" name="description" value={searchTags.description} onChange={() => { setSearchTags({ ...searchTags, description: !searchTags.description }); }} />
-              <label htmlFor="description">Description</label>
-            </div>
-            <div>
-              <input type="checkbox" id="title" name="title" value={searchTags.title} onChange={() => { setSearchTags({ ...searchTags, title: !searchTags.title }); }} />
-              <label htmlFor="title">Title</label>
-            </div>
-            <div>
-              <input type="checkbox" id="lost" name="lost" />
-              <label htmlFor="lost">Lost</label>
-            </div>
-            <div>
-              <input type="checkbox" id="date" name="date" value={searchTags.location} onChange={() => { setSearchTags({ ...searchTags, location: !searchTags.location }); }} />
-              <label htmlFor="date">Location</label>
-            </div>
-          </fieldset>
-        </div>
-        <div className='filter-box-spacing'>
-
-        </div>
-        <div className={'lostItems'}>
-          <div className="lostpage-banner">
-            <h1>Lost Items</h1>
+            {isLoading ? (
+              <div className="progressBox">
+                <CircularProgress />
+              </div>
+            ) : (
+              <Grid container={true} spacing={5}>
+                {lostItems.length === 0 || !lostItems ? (
+                  <div className="progressBox">
+                    <h3>No lost items found , Report a new item.</h3>
+                  </div>
+                ) : (
+                  lostItems.map((item, index) => (
+                    <Grid item={true} xs={12} md={6} lg={4} xl={4} key={index}>
+                      <Card
+                        item={item}
+                        refresh={getLostItems}
+                        setPopupTrigger={setPopupTrigger}
+                        setPopupData={setPopupData}
+                      />
+                      <Popup
+                        trigger={popupTrigger}
+                        setPopupTrigger={setPopupTrigger}
+                        data={popupData}
+                        alert={setIsAlert}
+                        success={setIsSuccess}
+                        message={setMessage}
+                      ></Popup>
+                    </Grid>
+                  ))
+                )}
+              </Grid>
+            )}
           </div>
-          {isLoading ? <div className='progressBox'><CircularProgress /></div> : (<Grid container={true} spacing={5}>
-            {lostItems.length === 0 || !lostItems ?
-              <div className='progressBox'>
-                <h3>No lost items found , Report a new item.</h3>
-              </div> :
-              (lostItems.map((item, index) => (
-                <Grid item={true} xs={12} md={6} lg={4} xl={4} key={index}>
-                  <Card item={item}
-                    refresh={getLostItems}
-                    setPopupTrigger={setPopupTrigger}
-                    setPopupData={setPopupData} />
-                  <Popup trigger={popupTrigger}
-                    setPopupTrigger={setPopupTrigger}
-                    data={popupData}
-                    alert={setIsAlert}
-                    success={setIsSuccess}
-                    message={setMessage}  >
-                  </Popup>
-                </Grid>
-              ))
-              )
-            }
-          </Grid>)}
-
         </div>
-      </div>
-    </section>
-  </>
+      </section>
+    </>
   );
 };
 
